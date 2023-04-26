72838514e471bfec59251af2ee889abcbf2978b7
mdforhan92 <mdforhan92@gmail.com>
2023-04-26 12:31:18 +0600

    deploy firebase

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,17 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProviders';
 
 const Login = () => {
-    const {signIn} = useContext(AuthContext);
+    const [show, setShow] = useState(false);
+
+
+    const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     console.log(location)
 
     const from = location.state?.from?.pathname || '/';
 
-    const handleLogin = event =>{
+    const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
@@ -19,16 +22,16 @@ const Login = () => {
         console.log(email, password)
 
         signIn(email, password)
-            .then(result =>{
+            .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
                 form.reset();
                 navigate(from, { replace: true })
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error);
             })
-        
+
     }
     return (
         <div className='form-container'>
@@ -40,7 +43,12 @@ const Login = () => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" required />
+                    <input type={show ? "text" : "password"} name="password" required />
+                    <p onClick={() => setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span> : <span>Show password</span>
+                        }
+                    </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
 
